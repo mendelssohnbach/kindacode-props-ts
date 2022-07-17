abe22f5458a16e7bc0598b260275f094271dd2fc
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-07-17 14:34:24 +0900

    :card_file_box: データを専用ファイルに切り出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import './App.css';
-import User, { Role } from './components/User';
-
-const user = {
-  id: 'u1',
-  email: 'johon.doe@gmail.com',
-  age: 100,
-  isMarried: false,
-  role: Role.member,
-  skills: ['React', 'Node.js', 'PostgreSQL'],
-  doSomething: () => {
-    console.log(`Hello ${user.email}`);
-  },
-};
+import User from './components/User';
+import user from './data/data';
 
 function App() {
   return (
---
+++ src/data/data.ts
@@ -0,0 +1,15 @@
+import User, { Role } from '../components/User';
+
+const user = {
+  id: 'u1',
+  email: 'johon.doe@gmail.com',
+  age: 100,
+  isMarried: false,
+  role: Role.member,
+  skills: ['React', 'Node.js', 'PostgreSQL'],
+  doSomething: () => {
+    console.log(`Hello ${user.email}`);
+  },
+};
+
+export default user;
